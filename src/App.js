ac9d036e3b18b253302c8a6e672d6b1c91505785
Jay <jayp007@gmail.com>
2019-06-07 06:39:15 -0500

    Cleanup.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React, { Component, Fragment } from 'react';
 import './App.css'; // global CSS
 
 class App extends Component {
-  show_me() { return 'This is it'; }
   render() {  // Lifecycle method
-    const name = 'Jack Spratt'
-    const show_me_too = () => 'It\'s a gas';
     return (
       <Fragment>
-        <h1>Hello, {this.show_me()}</h1>
-        <h2>Test, {show_me_too()}</h2>
+        <h1>Hello</h1>
       </Fragment>
     );
   }
