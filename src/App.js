1b0eadbcb5c5ce78571e086bb7dc9e4ba6b9f75e
Jay <jayp007@gmail.com>
2019-06-09 13:18:36 -0500

    interface with github api to get list of 30 users, pass as properties to Users component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  componen
+  state = {
+    users: [],
+    loading: false // set true once loaded
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true});  // cant simply use state.loading directly
+    const res = await axios.get('https://api.github.com/users?client_id=6eb89c1cef55190c2a11&client_secret=c5a3a3720db41bee7035836578d0e52e6485da29');
+    this.setState({ users: res.data, loading: false } );
+    // console.log(res.data);
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar   title="Github Finder (JGP)" icon="fab fa-github"/>
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,38 +2,16 @@ import React, { Component } from 'react'
 import UserItem from './UserItem';
 
 export class Users extends Component {
-    state = {  //hard-coded list of objects, eventually from github rest api
-        users: [
-          {
-            id: '1',
-            login: 'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/mojombo'
-          },
-          {
-            id: '2',
-            login: 'defunkt',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-            html_url: 'https://github.com/defunkt'
-          },
-          {
-            id: '3',
-            login: 'pjhyett',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-            html_url: 'https://github.com/pjhyett'
-          }
-        ]
-      };
-    
-      render() {
+    // props with users (list) and loading attributes
+    render() {
         return (
-          <div style={userStyle}>
-            {this.state.users.map(user => (
-              <UserItem key={user.id} user={user} />
-            ))}
-          </div>
+            <div style={userStyle}>
+                {this.props.users.map(user => (
+                  <UserItem key={user.id} user={user} />
+                 ))}
+            </div>
         );
-      }
+    }
 }
 
 const userStyle = {
