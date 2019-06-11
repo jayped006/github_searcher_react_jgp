52914cd564b42b4c6d78e05e56f39f296ad8b6f6
Jay <jayp007@gmail.com>
2019-06-11 01:29:02 -0500

    L22

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false, // true while async loading in place ==> Spinner
     alert: null // console.log analog, when not null -- object with msg, type
   }
@@ -24,6 +26,16 @@ class App extends Component {
       // Note: guaranteed to have res.data.items because of 'await'
   };
 
+   // Get single Github user
+   getUser = async user => {
+    this.setState({ loading: true });
+
+    const api_url = `https://api.github.com/users/${user}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+    const res = await axios.get(api_url);
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -35,6 +47,8 @@ class App extends Component {
   };
 
   render() {
+    const { users, user, loading } = this.state;
+
     return (
       <Router>
       <div className='App'>
@@ -48,15 +62,26 @@ class App extends Component {
                   <Search
                     searchUsers={this.searchUsers}
                     clearUsers={this.clearUsers}
-                    showClear={this.state.users.length > 0 ? true : false}
+                    showClear={users.length > 0 ? true : false}
                     setAlert={this.setAlert}
                   />
-                  <Users loading={this.state.loading} users={this.state.users} />
+                  <Users loading={loading} users={users} />
               </Fragment>
             )} /> 
 
             <Route exact path='/about' component={About} />
-            
+
+            <Route exact path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+
           </Switch>
         </div>
       </div>
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,31 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const UserItem = (props) => {  // Requires props -- with login, avatar_url, html_url
-    const { login, avatar_url, html_url } = props.user; // Deconstruct props
-    // NOTE: local variables -- not prop or state attribute
+  const { login, avatar_url } = props.user; // Deconstruct props, no longer using html_url
+  // NOTE: local variables -- not prop or state attribute
 
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
       </div>
-    );
+    </div>
+  );
 }
 
 UserItem.propTypes = {
-    user: PropTypes.object.isRequired, // doesnt specify need for avatar_url, etc
+  user: PropTypes.object.isRequired, // doesnt specify need for avatar_url, etc
 }
 
 export default UserItem
